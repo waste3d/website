e70ff848cf778c28b894ec5f38940c7978af3977
waste3d <kolya@waste3d.su>
2025-05-01 19:26:16 +0300

    tsparticles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,31 @@ module.exports = {
       './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
     ],
     theme: {
-      extend: {},
-    },
+      extend: {
+        keyframes: {
+          typing: {
+            "0%": {
+              width: "0%",
+              visibility: "hidden"
+            },
+            "100%": {
+              width: "100%"
+            }
+          },
+          blink: {
+            "50%": {
+              borderColor: "transparent"
+            },
+            "100%": {
+              borderColor: "white"
+            }
+          }
+        },
+        animation: {
+          typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+        },
+      },
+
     plugins: [],
-  }
+      },
+    }
